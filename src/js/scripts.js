7a5063c9c5bc3a5cfe5ee18e5272cf83349d4ef2
Andrew Chen <andrew8062@gmail.com>
2018-07-09 17:43:39 +0800

    update compare algorithem
    1. update compare algorithem
    2. update variable initial from var to let

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -19,10 +19,11 @@ var job_max_day = function(jobs){
 }
 
 function compare(a,b){
-		if (a.day == max_day) {return 1}
-		if(a.fetal && !b.fetal) {return -1}
-		if (a.day < b.day) { return -1}
+		// if (a.day == max_day) {return 1}
 		if (a.day > b.day) { return 1}
+		if (a.day < b.day) { return -1}
+		if(a.fetal && !b.fetal) {return -1}
+
 		return 0;
 	}
 
@@ -33,7 +34,7 @@ var system = function(id, running, available){
 	this.history = []
 }
 
-default_jobs = []
+let default_jobs = []
 default_jobs.push(new job("temperature humidity test", 10,4, true))
 default_jobs.push(new job("System Pogo", 7,2))
 default_jobs.push(new job("Weighted Shock", 1,2))
@@ -55,9 +56,10 @@ default_jobs.push(new job("vibration", 2,2,true))
 default_jobs.push(new job("Strain test", 2,1,true))
 default_jobs.push(new job("Palm rest vibration", 1,2))
 default_jobs.push(new job("Hinge cycle abrasion", 3,3))
-let max_day = job_max_day(default_jobs)
 
-html = ''
+let max_day = job_max_day(default_jobs)
+let html = ''
+
 html +=`
 <div class="divTableRow">
     <div class="divTableCell">Name</div>
@@ -67,8 +69,8 @@ html +=`
     <div class="divTableCell">Destoryed</div>
   </div>
 `
-for (var i in default_jobs){
-	fetal = default_jobs[i].fetal ? "checked" :""
+for (let i in default_jobs){
+	let fetal = default_jobs[i].fetal ? "checked" :""
 	html+=
 	`
 	<div class="divTableRow" id=`+ i +`>
@@ -81,17 +83,15 @@ for (var i in default_jobs){
 	  `
 }
 
-// console.log(html)
 document.getElementById('list').innerHTML = html;
 var update = function(){
-	select = document.getElementsByClassName('selector')
-	day = document.getElementsByClassName('day')
-	unit = document.getElementsByClassName('unit')
-	fetal = document.getElementsByClassName('destoryed')
+	let select = document.getElementsByClassName('selector')
+	let day = document.getElementsByClassName('day')
+	let unit = document.getElementsByClassName('unit')
+	let fetal = document.getElementsByClassName('destoryed')
 
-	for (var i in default_jobs){
+	for (let i in default_jobs){
 		default_jobs[i].day = parseInt(day[i].value)
-
 		default_jobs[i].unit = parseInt(unit[i].value)
 		default_jobs[i].fetal = fetal[i].checked
 	}
@@ -106,12 +106,14 @@ var start = function(){
 		default_systems.push(new system(i, 0, true))
 	}
 	//retrive data from original dataset
-	jobs = JSON.parse(JSON.stringify(default_jobs))
-	systems = JSON.parse(JSON.stringify(default_systems))
+	let jobs = JSON.parse(JSON.stringify(default_jobs))
+	let systems = JSON.parse(JSON.stringify(default_systems))
 	//Using shuffle or static sort
 	// shuffleArray(jobs)
 	jobs.sort(compare)
-
+	for (let i in jobs){
+		// console.log(jobs[i])
+	}
 	//check if exist a system that still running
 	let running_systems = function(){
 		for (var i in systems){
@@ -124,8 +126,8 @@ var start = function(){
 
 	//find minmim of n number of available systems
 	let find_empty_system = function(num=1){
-		available = []
-		for (var i in systems){
+		let available = []
+		for (let i in systems){
 			s = systems[i]
 			if (s.running == 0 && s.available){
 				available.push(s)
@@ -139,7 +141,7 @@ var start = function(){
 
 	//let all systems run of 1 day
 	let running = function(){
-		for (var i in systems){
+		for (let i in systems){
 			if (systems[i].running > 0){
 				systems[i].running--
 			}
@@ -155,22 +157,22 @@ var start = function(){
 		//keep running until all jobs are done and no systems are running for remaining jobs
 		while (jobs.length > 0 || running_systems()){
 			//loop jobs backward
-			for (var i = jobs.length-1; i>=0; i--){
-				
-				job = jobs[i]
+			for (let i = jobs.length-1; i>=0; i--){
+				let job = jobs[i]
+				// console.log(job)
 				//get empty systems
 				available_systems = find_empty_system(job.unit)
 				if (available_systems){
-					for (var j in available_systems){
+					for (let j in available_systems){
 						s = available_systems[j]
 						s.running = job.day
 						//if the job will damage system, system will be marked as not avaiable
 						if (job.fetal){
 							s.available = false
 							s.history.push([job.name+"(D)", day, job.day])
-						}else {s.history.push([job.name, day, job.day])
-}
-					}
+						}else {s.history.push([job.name, day, job.day])}
+
+					}					
 					jobs.pop()
 				}
 				//if no availabe systems, means all of them are doing jobs. skip one day for it to run
@@ -189,9 +191,9 @@ var start = function(){
 	 * Using Durstenfeld shuffle algorithm.
 	 */
 	function shuffleArray(array) {
-	    for (var i = array.length - 1; i > 0; i--) {
-	        var j = Math.floor(Math.random() * (i + 1));
-	        var temp = array[i];
+	    for (let i = array.length - 1; i > 0; i--) {
+	        let j = Math.floor(Math.random() * (i + 1));
+	        let temp = array[i];
 	        array[i] = array[j];
 	        array[j] = temp;
 	    }
@@ -208,11 +210,9 @@ var run1000 = function(n)
 	//update current value from HTML
 	update()
 	let resultHTML = ""
-	let day = 0
-	
-	result = start().simulator()
-	day = result[0]
-	systems = result[1]
+	let result = start().simulator()
+	let day = result[0]
+	let systems = result[1]
 
 	systems = JSON.parse(systems)
 	output_google_chart_format(systems)
@@ -223,14 +223,14 @@ var run1000 = function(n)
 
 
 var output_google_chart_format = function(systems){
-	google_chart_data = []
+	let google_chart_data = []
 	for (let i in systems){
-		system_histories = systems[i].history
+		let system_histories = systems[i].history
 		for (let j in system_histories){
 				let logs = system_histories[j]
 				if (logs[0] != "DESTORYED"){
 					let id = i
-					ms_to_day = 86400000
+					let ms_to_day = 86400000
 					let start = logs[1]*ms_to_day
 					let end = start + logs[2]*ms_to_day
 					google_chart_data.push([id, logs[0], start, end])					
@@ -245,9 +245,9 @@ var output_google_chart_format = function(systems){
   	google.charts.load("current", {packages:["timeline"]});
   	google.charts.setOnLoadCallback(drawChart);
   	function drawChart() {
-	    var container = document.getElementById('waterfall');
-	    var chart = new google.visualization.Timeline(container);
-	    var dataTable = new google.visualization.DataTable();
+	    let  container = document.getElementById('waterfall');
+	    let  chart = new google.visualization.Timeline(container);
+	    let dataTable = new google.visualization.DataTable();
 
 	    dataTable.addColumn({ type: 'string', id: 'Term' });
 	    dataTable.addColumn({ type: 'string', id: 'Name' });
@@ -256,6 +256,6 @@ var output_google_chart_format = function(systems){
 
 	    dataTable.addRows(chart_data);
 		
-	    chart.draw(dataTable);
+	    chart.draw(dataTable)
   }
 }
